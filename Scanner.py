39ddaa861dcab9b821f4e8c98126a5c0ea0763a2
DirectVenom <nmajchrzak77@gmail.com>
2025-03-22 05:00:29 +0000

    simple stuff complete

--- Scanner.py
+++ Scanner.py
@@ -65,3 +65,68 @@ class WebSecurityScanner:
                             'parameter': param,
                             'payload': payload
                         })
+            except Exception as e:
+                print(f"Error testing SQL injection on {url}: {str(e)}")
+    # Cross-site Scripting Check
+    
+    def check_xss(self, url: str) -> None:
+        xss_payloads = [
+            "<script>alert('XSS')</script>",
+            "<img src=x onerror=alert('XSS')>",
+            "javascript:alert('XSS)"
+        ]
+
+        for payload in xss_payloads:
+            try:
+                parsed = urllib.parse.urlparsse(url)
+                params = urllib.parse.parse_qs(parsed.query)
+
+                for param in params:
+                    test_url = url.replace(f"{param}={params[param][0]}", 
+                                 f"{param}={urllib.parse.quote(payload)}")
+                    response = self.session.get(test_url)
+
+                    if payload in response.text:
+                        self.report_vulnerability({
+                         'type': 'Cross-Site Scripting (XSS)',
+                            'url': url,
+                            'parameter': param,
+                            'payload': payload
+                        })
+
+            except Exception as e:
+                print(f"Error testing XSS on {url}: {str(e)}")
+
+    # Main scanner logic 
+    def scan(self) -> List[Dict]:
+        print(f"\n{colorama,Fore.BLUE} {self.target_url}{colorama.style.RESET_ALL}\n")
+
+        self.crawl(self.target_url) # Crawling the website
+        with ThreadPoolExecutor(max_workers=5) as excutor: # Running securtity checks on Urls.
+            for url in self.visited_urls:
+                excutor.submit(self.check_sql_injection, url)
+                excutor.submit(self.check_xss, url)
+
+            
+        return self.vulnerabilities
+    
+    def report_vulnerability(self, vulnerability: Dict) -> None:
+        self.vulnerabilities.append(vulnerability)
+        print(f"{colorama.Fore.RED}[VULNERABILITY FOUND]{colorama.Style.RESET_ALL}")
+        for key, value in vulnerability.items():
+            print(f"{key}: {value}")
+            print()
+
+    if __name__ == "__main__":
+        if len(sys.argv) != 2:
+            print("Usage: python scanner.py <target_url>")
+            sys.exit(1)
+
+        target_url = sys.argv[1]
+        scanner = WebSecurityScanner(target_url)
+        vulnerabilites = scanner.scan()
+
+
+        print(f"\n{colorama.Fore.GREEN}Scan Complete!{colorama.Style.RESET_ALL}")
+        print(f"Total URLs scanned: {len(scanner.visited_urls)}")
+        print(f"Vulnerabilities found: {len(vulnerabilities)}")
